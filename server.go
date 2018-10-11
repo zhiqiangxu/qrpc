d2850cee6fc8cd8c9e10d9e5a5c7609f4f7ed362
徐志强 <xuzhiqiang@innotechx.com>
2018-10-11 20:50:22 +0800

    mod SetKeepAlivePeriod

--- server.go
+++ server.go
@@ -165,7 +165,7 @@ func (srv *Server) serve(l tcpKeepAliveListener, idx int) error {
 	defer cancelFunc()
 	for {
 		l.SetDeadline(time.Now().Add(defaultAcceptTimeout))
-		rw, e := l.AcceptTCP()
+		rw, e := l.Accept()
 		if e != nil {
 			select {
 			case <-srv.doneChan:
@@ -212,7 +212,7 @@ func (ln tcpKeepAliveListener) Accept() (net.Conn, error) {
 		return nil, err
 	}
 	tc.SetKeepAlive(true)
-	tc.SetKeepAlivePeriod(3 * time.Minute)
+	tc.SetKeepAlivePeriod(20 * time.Second)
 	return tc, nil
 }
 
