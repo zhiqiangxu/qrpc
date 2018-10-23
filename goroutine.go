943098d530a11f79b819bb47ad80c47bec1376ee
徐志强 <xuzhiqiang@innotechx.com>
2018-10-23 20:37:06 +0800

    try ants

--- goroutine.go
+++ goroutine.go
@@ -2,13 +2,19 @@ package qrpc
 
 import (
 	"sync"
+
+	"github.com/panjf2000/ants"
 )
 
 // GoFunc runs a goroutine under WaitGroup
 func GoFunc(routinesGroup *sync.WaitGroup, f func()) {
 	routinesGroup.Add(1)
-	go func() {
+	err := ants.Submit(func() error {
 		defer routinesGroup.Done()
 		f()
-	}()
+		return nil
+	})
+	if err != nil {
+		logError("ants Submit", err)
+	}
 }
