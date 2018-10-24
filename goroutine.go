7936b3a6aa4ea62d8bdab3922734004eefec80fd
徐志强 <xuzhiqiang@innotechx.com>
2018-10-24 11:20:46 +0800

    Revert "try ants"
    This reverts commit 943098d530a11f79b819bb47ad80c47bec1376ee.

--- goroutine.go
+++ goroutine.go
@@ -2,19 +2,13 @@ package qrpc
 
 import (
 	"sync"
-
-	"github.com/panjf2000/ants"
 )
 
 // GoFunc runs a goroutine under WaitGroup
 func GoFunc(routinesGroup *sync.WaitGroup, f func()) {
 	routinesGroup.Add(1)
-	err := ants.Submit(func() error {
+	go func() {
 		defer routinesGroup.Done()
 		f()
-		return nil
-	})
-	if err != nil {
-		logError("ants Submit", err)
-	}
+	}()
 }
