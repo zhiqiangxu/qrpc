c8755a1475d46b36120f5e0a5181c10b2a44155b
徐志强 <shore.cloud@gmail.com>
2018-11-21 14:15:39 +0800

    add more hack

--- hack.go
+++ hack.go
@@ -1,5 +1,10 @@
 package qrpc
 
+import (
+	"reflect"
+	"unsafe"
+)
+
 func getg() *g
 
 type stack struct {
@@ -21,4 +26,24 @@ type g struct {
 func StackSize() uintptr {
 	g := getg()
 	return g.stack.hi - g.stack.lo
+}
+
+// Slice does no copy to change string to slice
+func Slice(s string) (b []byte) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pbytes.Data = pstring.Data
+	pbytes.Len = pstring.Len
+	pbytes.Cap = pstring.Len
+	return
+}
+
+// String does no copy to change slice to string
+// use your own risk
+func String(b []byte) (s string) {
+	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
+	pstring := (*reflect.StringHeader)(unsafe.Pointer(&s))
+	pstring.Data = pbytes.Data
+	pstring.Len = pbytes.Len
+	return
 }
