473878d9f82aaf42b5f501dd77d37eabef39a694
ouerqiang <ouerqiang@luojilab.com>
2019-12-29 00:30:35 +0800

    fix GoFunc panic

--- goroutine.go
+++ goroutine.go
@@ -2,12 +2,21 @@ package qrpc
 
 import (
 	"sync"
+	"runtime"
+
+	"go.uber.org/zap"
 )
 
 // GoFunc runs a goroutine under WaitGroup
 func GoFunc(routinesGroup *sync.WaitGroup, f func()) {
 	routinesGroup.Add(1)
+	_, file, line, _ := runtime.Caller(1)
 	go func() {
+		defer func() {
+			if err := recover();err != nil {
+				l.Error("GoFunc panic", zap.String("file", file), zap.Int("line", line))
+			}
+		}()
 		defer routinesGroup.Done()
 		f()
 	}()
