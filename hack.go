d25c97acc3e2a2209f449b6268d3d44ea0b4e428
徐志强 <xuzhiqiang@qutoutiao.net>
2020-02-11 15:10:15 +0800

    move StackSize to hack_amd64.go

--- hack.go
+++ hack.go
@@ -5,29 +5,6 @@ import (
 	"unsafe"
 )
 
-func getg() *g
-
-type stack struct {
-	lo uintptr
-	hi uintptr
-}
-type g struct {
-	// Stack parameters.
-	// stack describes the actual stack memory: [stack.lo, stack.hi).
-	// stackguard0 is the stack pointer compared in the Go stack growth prologue.
-	// It is stack.lo+StackGuard normally, but can be StackPreempt to trigger a preemption.
-	// stackguard1 is the stack pointer compared in the C stack growth prologue.
-	// It is stack.lo+StackGuard on g0 and gsignal stacks.
-	// It is ~0 on other goroutine stacks, to trigger a call to morestackc (and crash).
-	stack stack // offset known to runtime/cgo
-}
-
-// StackSize calculates current stack size
-func StackSize() uintptr {
-	g := getg()
-	return g.stack.hi - g.stack.lo
-}
-
 // Slice does no copy to change string to slice
 func Slice(s string) (b []byte) {
 	pbytes := (*reflect.SliceHeader)(unsafe.Pointer(&b))
