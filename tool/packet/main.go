14ca6df4eb3a03801a90d0380b1833eb1eb9484d
徐志强 <652732310@qq.com>
2020-02-21 13:10:01 +0800

    add packet tool

---
+++ tool/packet/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"encoding/binary"
+	"flag"
+	"fmt"
+	"log"
+	"os"
+)
+
+var (
+	cmd       = flag.Int("c", 0, "cmd for qrpc")
+	flg       = flag.Int("f", 0, "flag for qrpc")
+	requestID = flag.Uint64("r", 0, "request id for qrpc")
+	payload   = flag.String("p", "", "payload for qrpc")
+)
+
+func main() {
+	flag.Usage = func() {
+		fmt.Fprintf(os.Stderr, "Usage: %s [options]\n", os.Args[0])
+		flag.PrintDefaults()
+	}
+
+	flag.Parse()
+
+	if *cmd < 0 || *cmd > 0xffffff {
+		log.Fatal("invalid cmd")
+	}
+
+	if *flg < 0 || *flg > 0xff {
+		log.Fatal("invalid cmd")
+	}
+
+	buf := make([]byte, 16+len(*payload))
+
+	length := len(buf) - 4
+	binary.BigEndian.PutUint32(buf, uint32(length))
+	binary.BigEndian.PutUint64(buf[4:], *requestID)
+	cmdAndFlags := uint32(*flg)<<24 + uint32(*cmd)&0xffffff
+	binary.BigEndian.PutUint32(buf[12:], cmdAndFlags)
+	copy(buf[16:], []byte(*payload))
+
+	fmt.Printf("packet is: %x\n", buf)
+}
